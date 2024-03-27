21c5d998619964cffaedd2a3343ac5dda08b49fe
Thiago Sanches Hohlenwerger <thiagosanchess52@gmail.com>
2024-03-27 16:13:39 -0300

    removido a aba de curriculo

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function redirectToPage(pageURL){
-    window.location.href = pageURL
+function redirectToPage(pageURL) {
+  window.location.href = pageURL;
 }
+
+// function downloadPDF() {
+//   const urlPDF = "../Portifolio/assets/Thiago Sanches Hohlenwerger.pdf"
+//   document.getElementById("downloadLink").href = urlPDF;
+
+//   document
+//     .getElementById("downloadLink")
+//     .setAttribute("download", "Thiago Sanches Hohlenwerger.pdf");
+// }
